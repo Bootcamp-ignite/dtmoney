795c7aafe2ce41f24dbe268287b2c1d027eb6979
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-03-16 14:32:30 -0300

    configured axios and miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { GlobalStyle } from "./styles/global";
+import { createServer } from 'miragejs';
+
+createServer({
+  namespace: 'api',
+  routes() {
+    this.get("/transactions", () => {
+      return [{
+        id: 1,
+        type: "income",
+        title: "Desenvolvimento de site",
+        price: 120000,
+        category: 'vendas',
+        createdAt: new Date()
+      }]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-
     <App />
   </React.StrictMode>,
   document.getElementById("root")
